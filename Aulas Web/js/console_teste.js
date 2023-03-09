1f90af81f5765844aace35f8705d184d633a1b62
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-09 17:44:26 -0300

    Update console_teste.js

--- Aulas Web/js/console_teste.js
+++ Aulas Web/js/console_teste.js
@@ -1,8 +1,8 @@
 /* var nome = window.prompt("Qual seu nome?");
 console.log("Prazer, " + nome);
-console.log(`Prazer, ${nome}`);
+console.log(`Prazer, ${nome}`); */
 
-var numero = 0; //tipo número
+/* var numero = 0; //tipo número
 var num = "0"; //tipo string
 console.log(numero == num); //compara valores
 console.log(numero === num); //compara valores e tipos */
@@ -34,15 +34,42 @@ switch (favoriteColor) {
     break;
   default:
     console.log("Invalid Color");
-}
+} */
 
-for(let i = 0; i < 10; i++){
+/* for(let i = 0; i < 10; i++){
     console.log(i)
 } */
 
-let verdade = false
+/* let verdade = false
 
 while (verdade < 10){
     console.log("Oi")
     verdade++
+} */
+
+/* function soma(a, b){
+  console.log(`A soma de ${a} + ${b} é: ${a+b}`)
 }
+
+soma(2,3)
+soma(10,11)
+soma(9.5,7.2) */
+
+/* function digaOi(){
+  console.log("Oi")
+}
+
+digaOi() */
+
+/* function multiplica(x, y){
+  return x*y
+}
+
+let valor = multiplica(2,4)
+console.log(valor) */
+
+let nome = "fellipe"
+console.log(nome.toLocaleUpperCase())
+console.log(nome.length)
+console.log(nome.slice(0,2))
+console.log(nome[0])

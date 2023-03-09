cc3170db210e8814ab7b56575910dc9a60ed7e72
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-09 16:14:48 -0300

    Novos conceito Javascript

--- Aulas Web/js/console_teste.js
+++ Aulas Web/js/console_teste.js
@@ -1,2 +1,37 @@
-var nome = window.prompt("Qual seu nome?")
-console.log("Prazer, " + nome)
+/* var nome = window.prompt("Qual seu nome?");
+console.log("Prazer, " + nome);
+console.log(`Prazer, ${nome}`);
+
+var numero = 0; //tipo número
+var num = "0"; //tipo string
+console.log(numero == num); //compara valores
+console.log(numero === num); //compara valores e tipos */
+
+/* var idade = window.prompt("Qual é sua idade?")
+
+if(idade >= 18){
+    console.log("Você é maior de idade, já pode ser preso")
+} */
+
+/* var idade = window.prompt("Qual é sua idade?");
+
+if (idade >= 18 && idade < 70) {
+  console.log("Seu voto é obrigatório");
+} else if ((idade >= 16 && idade < 18) || idade >= 70){
+    console.log("Seu voto é opcional")
+} else {        
+    console.log("Você não pode votar ainda!")
+} */
+
+let favoriteColor = "red"
+        
+switch (favoriteColor) {
+  case "red":
+    console.log("Your favorite color is red");
+    break;
+  case "green":
+    console.log("Your favorite color is green");
+    break;
+  default:
+    console.log("Invalid Color");
+}

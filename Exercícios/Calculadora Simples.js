478b199db209a2ebd6a2c7b0cec2f1fb6cf0fc30
DaronBecaus <danielsilvasoder@gmail.com>
2023-03-10 20:19:48 -0300

    Create Calculadora Simples.js

---
+++ Exercícios/Calculadora Simples.js
@@ -0,0 +1,4 @@
+let num1 = window.prompt("Escreva o primeiro número:")
+let num2 = window.prompt("Escreva o segundo número:")
+console.log(`A soma de ${num1} e ${num2} é igual á: ${num1+num2}`)
+console.log(`A subtração de ${num1} e ${num2} é igual á: ${num1-num2}`)

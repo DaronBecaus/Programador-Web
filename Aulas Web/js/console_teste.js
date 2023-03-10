e8fd3116ca0627d5603fdcdf199af8d4774f09eb
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-09 21:40:32 -0300

    Update console_teste.js

--- Aulas Web/js/console_teste.js
+++ Aulas Web/js/console_teste.js
@@ -1,29 +1,29 @@
 /* var nome = window.prompt("Qual seu nome?");
 console.log("Prazer, " + nome);
-console.log(`Prazer, ${nome}`); */
+console.log(`Prazer, ${nome}`);
 
-/* var numero = 0; //tipo número
+var numero = 0; //tipo número
 var num = "0"; //tipo string
 console.log(numero == num); //compara valores
-console.log(numero === num); //compara valores e tipos */
+console.log(numero === num); //compara valores e tipos
 
-/* var idade = window.prompt("Qual é sua idade?")
+var idade = window.prompt("Qual é sua idade?");
 
-if(idade >= 18){
-    console.log("Você é maior de idade, já pode ser preso")
-} */
+if (idade >= 18) {
+  console.log("Você é maior de idade, já pode ser preso");
+}
 
-/* var idade = window.prompt("Qual é sua idade?");
+var idade = window.prompt("Qual é sua idade?");
 
 if (idade >= 18 && idade < 70) {
   console.log("Seu voto é obrigatório");
-} else if ((idade >= 16 && idade < 18) || idade >= 70){
-    console.log("Seu voto é opcional")
-} else {        
-    console.log("Você não pode votar ainda!")
-} */
+} else if ((idade >= 16 && idade < 18) || idade >= 70) {
+  console.log("Seu voto é opcional");
+} else {
+  console.log("Você não pode votar ainda!");
+}
 
-/* let favoriteColor = "red"
+let favoriteColor = "red";
 
 switch (favoriteColor) {
   case "red":
@@ -34,46 +34,54 @@ switch (favoriteColor) {
     break;
   default:
     console.log("Invalid Color");
-} */
-
-/* for(let i = 0; i < 10; i++){
-    console.log(i)
-} */
-
-/* let verdade = false
-
-while (verdade < 10){
-    console.log("Oi")
-    verdade++
-} */
-
-/* function soma(a, b){
-  console.log(`A soma de ${a} + ${b} é: ${a+b}`)
 }
 
-soma(2,3)
-soma(10,11)
-soma(9.5,7.2) */
-
-/* function digaOi(){
-  console.log("Oi")
+for (let i = 0; i < 10; i++) {
+  console.log(i);
 }
 
-digaOi() */
+let verdade = false;
 
-/* function multiplica(x, y){
-  return x*y
+while (verdade < 10) {
+  console.log("Oi");
+  verdade++;
 }
 
-let valor = multiplica(2,4)
-console.log(valor) */
+function soma(a, b) {
+  console.log(`A soma de ${a} + ${b} é: ${a + b}`);
+}
+
+soma(2, 3);
+soma(10, 11);
+soma(9.5, 7.2);
+
+function digaOi() {
+  console.log("Oi");
+}
+
+digaOi();
+
+function multiplica(x, y) {
+  return x * y;
+}
+
+let valor = multiplica(2, 4);
+console.log(valor);
 
 // algumas funções internas de js para manipular Strings
-let nome = "fellies"
-console.log(nome.toLocaleUpperCase()) //todas maiúsculas, função oposta = toLowerCase()
-console.log(nome.length) //comprimento da string (também usada em arrays)
-console.log(nome.slice(0,2)) //recorte
-console.log(nome[0]) //retorna o valor pela posição (usada em arrays, strings são tratadas como arrays)
+let nome = "fellies";
+console.log(nome.toLocaleUpperCase()); //todas maiúsculas, função oposta = toLowerCase()
+console.log(nome.length); //comprimento da string (também usada em arrays)
+console.log(nome.slice(0, 2)); //recorte
+console.log(nome[0]); //retorna o valor pela posição (usada em arrays, strings são tratadas como arrays)
 
-let frase = "Hello World"
-console.log(frase.replace("World", "peoples")) //substitui uma string por outra
+let frase = "Hello World";
+console.log(frase.replace("World", "peoples")); //substitui uma string por outra */
+
+let numero = "10";
+let valor = 12;
+let preco = 13.343535;
+
+console.log(typeof numero); //função geral para verificar o tipo de uma variável
+console.log(valor.toString()); //converte o valor de uma variável para tipo string
+console.log(preco.toFixed(2)); //exibe até uma quantidade definida de casas decimais

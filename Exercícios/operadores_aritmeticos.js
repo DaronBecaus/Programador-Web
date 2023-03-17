a130639279099cf66c48778c7f3a66e36fb8e75e
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-17 20:32:54 -0300

    Novos arquivos de exercícios

---
+++ Exercícios/operadores_aritmeticos.js
@@ -0,0 +1,6 @@
+var num1 = window.prompt("Digite o valor a: ");
+var num2 = window.prompt("Digite o valor b: ");
+console.log(`${num1} + ${num2} = ${num1 + num2}`);
+console.log(`${num1} - ${num2} = ${num1 - num2}`);
+console.log(`${num1} x ${num2} = ${num1 * num2}`);
+console.log(`${num1} / ${num2} = ${num1 / num2}`);
